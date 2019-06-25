10d62152771b530c010fb42fa683f7b5eba214f7
Sarah Drasner <sarah.drasner@gmail.com>
2019-06-25 10:54:22 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>
+      Hello tacoface
+      <br>I love delicious tacos
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Projects</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
